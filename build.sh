9716f557461d7a3662c46b45b2d502a02258a680
Brendan <baharmon@gmail.com>
2016-04-25 17:28:24 -0400

    edited build script

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 ./copy-common-files.py --dst-dir=build
 
 ./build-slides.py --outdir=build \
-    --title="Tangible Landscape &#64; TEI 2016" \
-    --meta-description="Embodied Cognition with Tangible Landscape" \
+    --title="Tangible Landscape" \
+    --meta-description="Tangible Landscape" \
     --meta-author="Brendan Harmon" \
     --outfile=index.html \
     index.html
